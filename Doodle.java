7773bbd980c18648b1f64e08c83fdfa0199bb71f
iamPraneetKaur <itspraneetkaur@gmail.com>
2025-07-10 04:04:04 +0530

    Added eraser, night mode, enhanced overall look, done and dusted

--- Doodle.java
+++ Doodle.java
@@ -1,51 +1,133 @@
 package Doodle;
+
 import Doodle.DrawingCanvas;
 import java.util.*;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
-class Doodle
-{
-	public static void main(String args[])
-	{	
-		JFrame frame= new JFrame("Doodle");
+class Doodle {
+	public static void main(String args[]) {
+		JFrame frame = new JFrame("Doodle");
 		frame.setSize(1500, 1000);
 		frame.setLayout(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
-	
-		//Ribbon
-		JPanel ribbon= new JPanel();
-		ribbon.setLayout(null);
-		ribbon.setBackground(Color.MAGENTA);
-		ribbon.setBounds(0, 0, 1500, 150);
-		//Color Button
-		JButton colorButton= new JButton("Color");
-		colorButton.setBounds(300, 30, 70, 70);
-		ribbon.add(colorButton);
-		//Pointer Button
-		JButton pointerButton= new JButton("Pointer");
-		pointerButton.setBounds(380, 30, 70, 70);
-		ribbon.add(pointerButton);
-		//Brush Button
-		JButton brushButton= new JButton("Brush");
-		brushButton.setBounds(460, 30, 70, 70);
-		ribbon.add(brushButton);
-		//Shape Button
-		JButton shapeButton= new JButton("Shape");
-		shapeButton.setBounds(540, 30, 70, 70);
-		ribbon.add(shapeButton);
-		
+		Font myFont= new Font("Comic Sans MS", Font.BOLD, 25);
+		ImageIcon paint= new ImageIcon(Doodle.class.getResource("paint.png"));
+		frame.setIconImage(paint.getImage());
+
+
+		JLabel header= new JLabel("Doodle");
+		header.setFont(new Font("Comic Sans MS", Font.BOLD, 100));
+		header.setBounds(20, 0, 500, 150);
+		frame.add(header);
+
 		DrawingCanvas canvas = new DrawingCanvas();
-		canvas.setBounds(0, 150, 1500, 850); // below the ribbon
+		canvas.setBounds(0, 150, 1500, 850);
 		frame.add(canvas);
+
 		
+		//Ribbon
+		JPanel ribbon = new JPanel();
+		ribbon.setLayout(null);
+		ribbon.setBackground(Color.decode("#FAB1CD"));
+		ribbon.setBounds(0, 0, 1500, 150);
+
+		//Night mode Button
+		JToggleButton toggle = new JToggleButton("☀");
+		toggle.setBounds(460, 30, 80, 80);
+		toggle.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 30));
+		toggle.setBackground(Color.decode("#ffd0e2"));
+		toggle.setFocusPainted(false);
+		// Behavior		
+		toggle.addItemListener(e -> {
+    		if (toggle.isSelected()) {
+        		toggle.setText("🌙");
+				canvas.darkMode();
+				
+    		}
+			else{
+        		toggle.setText("☀");
+        		canvas.lightMode();
+    		}
+		});
+		ribbon.add(toggle);
+
+		//Color Button
+		JButton colorButton = new JButton("🖍");
+		colorButton.setBounds(550, 30, 80, 80);
+		colorButton.setBackground(Color.decode("#ffd0e2"));
+		colorButton.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 30));
+		ribbon.add(colorButton);		
+		colorButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Color selectedColor = JColorChooser.showDialog(null, "Pick any color you like!", Color.BLACK);
+				if (selectedColor != null) {
+					canvas.setCurrentColor(selectedColor);
+				}
+			}
+		});
+		//Save Button
+		JButton saveButton = new JButton("Save");
+		saveButton.setBounds(640, 30, 80, 80);
+		saveButton.setBackground(Color.decode("#ffd0e2"));
+		saveButton.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+		ribbon.add(saveButton); 
+		//Listener
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				canvas.saveImageToFile();
+			}
+		});
+		
+		//Eraser Button
+		JButton eraserButton = new JButton("🧽");
+		eraserButton.setBounds(730, 30, 80, 80);
+		eraserButton.setBackground(Color.decode("#ffd0e2"));
+		eraserButton.setFont(new Font("Segoe UI Emoji", Font.PLAIN, 30));
+		ribbon.add(eraserButton);
+		//Listener
+		eraserButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e){
+				canvas.setCurrentColor(Color.WHITE);
+			}
+		}); 
+		// Clear Button
+		JButton clearButton = new JButton("Clear");
+		clearButton.setBounds(820, 40, 100, 50);
+		clearButton.setBackground(Color.decode("#ffe2ed"));
+		clearButton.setFont(myFont);
+		ribbon.add(clearButton);
+		clearButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				canvas.clearCanvas();
+			}
+		});
+
+		//Brush size
+		JLabel brushLabel = new JLabel("Pointer Size");
+		brushLabel.setBounds(1200, 20, 100, 20);
+		brushLabel.setBackground(Color.decode("#ffe2ed"));
+		ribbon.add(brushLabel);
+		// Slider for pointer size
+		JSlider brushSlider = new JSlider(1, 50, 2);
+		brushSlider.setBounds(1150, 40, 200, 50);
+		brushSlider.setMajorTickSpacing(10);
+		brushSlider.setMinorTickSpacing(1);
+		brushSlider.setPaintTicks(true);
+		brushSlider.setPaintLabels(true);
+		ribbon.add(brushSlider);
+		// Listener to update pointer size
+		brushSlider.addChangeListener(e -> {
+			int newSize = brushSlider.getValue();
+			canvas.setBrushSize(newSize);
+		});
+
+		
+
 		frame.add(ribbon);
 
-		
-		
-		frame.setVisible(true);	
+		frame.setVisible(true);
 	}
-
-	
 }
--- DrawingCanvas.java
+++ DrawingCanvas.java
@@ -1,17 +1,30 @@
 package Doodle;
 import java.util.*;
+import javax.imageio.ImageIO;
+import java.io.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 public class DrawingCanvas extends JPanel{
     BufferedImage canvasImage;
     int lastX, lastY;
     Color currentColor= Color.BLACK;
     
+    int brushSize = 2; // default brush size
+    public void setBrushSize(int size) {
+        this.brushSize = size;
+    }
+
+    public void setCurrentColor(Color color){
+        this.currentColor= color;
+    }
     public DrawingCanvas(){
-        canvasImage= new BufferedImage(800, 600, BufferedImage.TYPE_INT_ARGB);
+        canvasImage= new BufferedImage(1500, 900, BufferedImage.TYPE_INT_ARGB);
         clearCanvas();
+        
 
         //Mouse Press(storing starting point)
         addMouseListener(new MouseAdapter() {
@@ -28,13 +41,12 @@ public class DrawingCanvas extends JPanel{
 
                 Graphics2D g2 = canvasImage.createGraphics();
                 g2.setColor(currentColor);
-                g2.setStroke(new BasicStroke(2)); // brush thickness
+                g2.setStroke(new BasicStroke(brushSize)); // brush thickness
                 g2.drawLine(lastX, lastY, currentX, currentY);
                 g2.dispose();
 
                 lastX = currentX;
                 lastY = currentY;
-
                 repaint(); // refresh panel
             }
         });
@@ -42,9 +54,47 @@ public class DrawingCanvas extends JPanel{
     public void clearCanvas() {
         Graphics2D g2 = canvasImage.createGraphics();
         g2.setColor(Color.WHITE);
+        setCurrentColor(Color.BLACK);
         g2.fillRect(0, 0, canvasImage.getWidth(), canvasImage.getHeight());
         g2.dispose();
+        repaint();
     }
+    public void darkMode() {
+        Graphics2D g2 = canvasImage.createGraphics();
+        g2.setColor(Color.BLACK);
+        setCurrentColor(Color.WHITE);
+        g2.fillRect(0, 0, canvasImage.getWidth(), canvasImage.getHeight());
+        repaint();
+    }
+    public void lightMode() {
+        Graphics2D g2 = canvasImage.createGraphics();
+        g2.setColor(Color.WHITE);
+        setCurrentColor(Color.BLACK);
+        g2.fillRect(0, 0, canvasImage.getWidth(), canvasImage.getHeight());
+        repaint();
+    }
+    public void saveImageToFile() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setDialogTitle("Save Image");
+
+        int userSelection = fileChooser.showSaveDialog(this);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = fileChooser.getSelectedFile();
+            try{
+                // Add .png if not included
+                if (!fileToSave.getName().toLowerCase().endsWith(".png")) {
+                    fileToSave = new File(fileToSave.getAbsolutePath() + ".png");
+                }
+                ImageIO.write(canvasImage, "png", fileToSave);
+                JOptionPane.showMessageDialog(this, "Image saved successfully!");
+            } 
+            catch (IOException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(this, "Failed to save image: " + e.getMessage());
+            }
+        }
+    }
+
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
